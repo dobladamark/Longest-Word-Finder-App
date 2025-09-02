b93d95738f9bcdc2d812de9a6885c91c43f8914a
mark <122426238+dobladamark@users.noreply.github.com>
2025-09-02 16:43:17 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function findLongestWordLength(str) {
+
+  let words = str.split(" ");
+  let longest = 0;
+
+  for (let word of words) {
+    if(word.length > longest) {
+      longest = word.length;
+    }
+
+
+  }
+  return longest;
+}
+
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
